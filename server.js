2b09cb9254350f93da00c740dcc1a3287320c60b
martin <martin@tub3.uk>
2023-11-18 20:44:07 +0000

    temporary fix for vercel timeout issue

--- server.js
+++ server.js
@@ -14,15 +14,17 @@ app.prepare().then(() => {
   const server = express();
 
   // Create a rate limiter for /api/postoffice/deliver
-  const postOfficeDeliverRateLimiter = rateLimit({
-    windowMs: 12 * 60 * 60 * 1000, // 12 hours
-    max: 1, // limit each IP to 1 requests per windowMs, you'd have to be mental (or a bot) (or a mental bot) to send more than 1 email per 12 hours
-    handler: (req, res, next) => {
-        let file = path.join(process.cwd(), 'private', 'mailerror', 'limit.html');
-        let html = fs.readFileSync(file, 'utf8');
-        res.status(429).send(html);
-    },
-  });
+//   const postOfficeDeliverRateLimiter = rateLimit({
+//     windowMs: 12 * 60 * 60 * 1000, // 12 hours
+//     max: 1, // limit each IP to 1 requests per windowMs, you'd have to be mental (or a bot) (or a mental bot) to send more than 1 email per 12 hours
+//     handler: (req, res, next) => {
+//         let file = path.join(process.cwd(), 'private', 'mailerror', 'limit.html');
+//         let html = fs.readFileSync(file, 'utf8');
+//         res.status(429).send(html);
+//     },
+//   });
+
+// temporary fix for vercel timeout issue
 
   // Apply the rate limiter only to /api/postoffice/deliver
   server.use('/api/postoffice/deliver', postOfficeDeliverRateLimiter);
