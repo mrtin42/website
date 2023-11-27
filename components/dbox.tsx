6afe47162645f6a4f169d51a5f98bfb6e06cb3e9
martin <martin@tub3.uk>
2023-11-27 17:50:40 +0000

    add tailwind font assistant

--- components/dbox.tsx
+++ components/dbox.tsx
@@ -15,7 +15,7 @@ type DBoxProps = {
 export default function DBox(props: DBoxProps) {
     return (
         <div className={props.className} id={props.id} style={props.style} {...props.props}>
-            <Link href={['https://', props.domainlink].join('')} className='text-3xl text-center text-white font-bold'>{props.title}</Link>
+            <Link href={['https://', props.domainlink].join('')} className='text-3xl text-center text-white font-bold font-assistant'>{props.title}</Link>
             <div className='flex flex-col items-center justify-center w-full h-full p-4 space-y-4 rounded-xl'>
                 {props.children}
             </div>
