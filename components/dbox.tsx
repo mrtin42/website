990f35cb93cb686b8d674f8a1f441850fd0f3fc2
martin <martin@mbfrias.co.uk>
2024-01-19 12:57:39 +0000

    Update dbox.tsx

--- components/dbox.tsx
+++ components/dbox.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
+import { VT323 } from 'next/font/google';
 
+const vt = VT323({ subsets: ['latin'] });
 
 type DBoxProps = {
     title: string;
@@ -15,7 +17,7 @@ type DBoxProps = {
 export default function DBox(props: DBoxProps) {
     return (
         <div className={[props.className, '  bg-[#00000093] text-white'].join(' ')} id={props.id} style={props.style} {...props.props}>
-            <Link href={['https://', props.domainlink].join('')} className='text-3xl text-center text-white font-bold font-assistant'>{props.title}</Link>
+            <Link href={['https://', props.domainlink].join('')} className={['text-3xl text-center text-white font-bold font-assistant', vt.className].join(' ')}>{props.title}</Link>
             <div className='flex flex-col items-center justify-center w-full h-full p-4 space-y-4 rounded-xl'>
                 {props.children}
             </div>
