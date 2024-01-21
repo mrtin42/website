c7d6e889bb45f484e505fb3977f297c2716cd19b
Martin Banks Frias <martin@mbfrias.co.uk>
2024-01-21 23:26:05 +0000

    remove unnecessary code in Box component
    broke the blasted thing stuoidnduc fuf eurbdyneucebjdbebdbd

--- components/box.tsx
+++ components/box.tsx
@@ -13,20 +13,6 @@ type BoxProps = {
 export default function Box(props: BoxProps) {
     return (
         <div className={props.className} id={props.id} style={props.style} {...props.props}>
-            {
-                props.title !== 'LondonTransit' ? (
-                    <h1 className='text-3xl text-center text-white font-bold'>{props.title}</h1>
-                ) : (
-                    <Image height={50} width={100} alt={`${props.title} logo`} src={`https://fs1.euwest.martinservers.cloud/${props.title}.svg`} />
-                )
-            }
-            {
-                props.title !== 'FORMALISER' ? (
-                    <h1 className='text-3xl text-center text-white font-bold'>{props.title}</h1>
-                ) : (
-                    <Image height={50} width={100} alt={`${props.title} logo`} src={`https://fs1.euwest.martinservers.cloud/${props.title}.svg`} />
-                )
-            }
             <div className='flex flex-col items-center justify-center w-full h-full p-4 space-y-4 rounded-xl'>
                 {props.children}
             </div>
