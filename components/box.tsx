e3d0a6ebecfe1f7dfd13254ef5111dcfe7658b72
martin <martin@tub3.uk>
2024-01-31 21:44:07 +0000

    bring back project titles!!!!!!!!!

--- components/box.tsx
+++ components/box.tsx
@@ -13,6 +13,7 @@ type BoxProps = {
 export default function Box(props: BoxProps) {
     return (
         <div className={props.className} id={props.id} style={props.style} {...props.props}>
+            <h1 className='text-2xl font-bold text-center text-white'>{props.title}</h1>
             <div className='flex flex-col items-center justify-center w-full h-full p-4 space-y-4 rounded-xl'>
                 {props.children}
             </div>
