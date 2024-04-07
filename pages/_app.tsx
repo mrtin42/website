b1d018d372734b4e7c7c2c32e7989546dc70792d
martin <martin@tub3.uk>
2024-04-07 17:45:48 +0100

    add Cloudflare Web Analytics script to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      {`<!-- Cloudflare Web Analytics -->`}<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "197a6a106f2044acbd272f6662016c75"}'></script>{`<!-- End Cloudflare Web Analytics -->`}
       <Analytics />
     </>
   )
